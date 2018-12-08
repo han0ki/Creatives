706a0063d91296f7845c0168e2c6921975284b1e
han0ki <43035482+han0ki@users.noreply.github.com>
2018-12-08 22:38:35 +0300

    Add files via upload

---
+++ creatives site/js/script.js
@@ -0,0 +1,72 @@
+﻿$('a[href^="#lok2"]').bind('click.smoothscroll',function (e) {
+ e.preventDefault();
+ 
+var target = this.hash,
+ $target = $(target);
+ 
+$('html, body').stop().animate({
+ 'scrollTop': $target.offset().top
+ }, 1000, 'swing', function () {
+ window.location.hash = target;
+ });
+ });
+
+$('a[href^="#lok3"]').bind('click.smoothscroll',function (e) {
+ e.preventDefault();
+ 
+var target = this.hash,
+ $target = $(target);
+ 
+$('html, body').stop().animate({
+ 'scrollTop': $target.offset().top
+ }, 1000, 'swing', function () {
+ window.location.hash = target;
+ });
+ });
+
+$('a[href^="#lok4"]').bind('click.smoothscroll',function (e) {
+ e.preventDefault();
+ 
+var target = this.hash,
+ $target = $(target);
+ 
+$('html, body').stop().animate({
+ 'scrollTop': $target.offset().top
+ }, 2000, 'swing', function () {
+ window.location.hash = target;
+ });
+ });
+
+$('a[href^="#lok5"]').bind('click.smoothscroll',function (e) {
+ e.preventDefault();
+ 
+var target = this.hash,
+ $target = $(target);
+ 
+$('html, body').stop().animate({
+ 'scrollTop': $target.offset().top
+ }, 2000, 'swing', function () {
+ window.location.hash = target;
+ });
+ });
+
+$('a[href^="#lok6"]').bind('click.smoothscroll',function (e) {
+ e.preventDefault();
+ 
+var target = this.hash,
+ $target = $(target);
+ 
+$('html, body').stop().animate({
+ 'scrollTop': $target.offset().top
+ }, 1000, 'swing', function () {
+ window.location.hash = target;
+ });
+ });
+
+(function($){
+  $(function() {
+    $('.menuclass').on('click', function() {
+      $(this).closest('.menu').toggleClass('menu_state_open');
+    });
+  });
+})(jQuery);
